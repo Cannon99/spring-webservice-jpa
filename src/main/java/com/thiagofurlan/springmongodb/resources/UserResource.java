1f9bfa13240e815f871bfeffd257f7ef59f9d7af
Thiago Furlan <thiago_furlan@outlook.com>
2021-10-31 15:04:08 -0300

    Service layer, component registration

--- src/main/java/com/thiagofurlan/springmongodb/resources/UserResource.java
+++ src/main/java/com/thiagofurlan/springmongodb/resources/UserResource.java
@@ -1,19 +1,32 @@
 package com.thiagofurlan.springmongodb.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thiagofurlan.springmongodb.entities.User;
+import com.thiagofurlan.springmongodb.services.UserService;
 
 @RestController
 @RequestMapping(path = "/users")
 public class UserResource {
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> getUser() {
-		User user = new User(1L, "Rei", "rei@rei", "999", "1234");
+	public ResponseEntity<List<User>> getUser() {
+		List<User> users = service.findAll();
+		return ResponseEntity.ok(users);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable Long id) {
+		User user = service.findById(id);
 		return ResponseEntity.ok(user);
 	}
 
---
+++ src/main/java/com/thiagofurlan/springmongodb/services/UserService.java
@@ -0,0 +1,25 @@
+package com.thiagofurlan.springmongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagofurlan.springmongodb.entities.User;
+import com.thiagofurlan.springmongodb.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.get();	
+	}
+}
