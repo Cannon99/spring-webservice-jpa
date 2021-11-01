6b4d000224ad4f76b55d70621a2d412b27858d6c
Thiago Furlan <thiago_furlan@outlook.com>
2021-11-01 18:57:09 -0300

    OrderStatus enum

--- src/main/java/com/thiagofurlan/springmongodb/config/TestConfig.java
+++ src/main/java/com/thiagofurlan/springmongodb/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.thiagofurlan.springmongodb.entities.Order;
 import com.thiagofurlan.springmongodb.entities.User;
+import com.thiagofurlan.springmongodb.entities.enums.OrderStatus;
 import com.thiagofurlan.springmongodb.repositories.OrderRepository;
 import com.thiagofurlan.springmongodb.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
 		User user1 = new User(null, "Rei", "rei@rei", "9999", "1234");
 		User user2 = new User(null, "Cannón", "cannon@cannon", "9999", "1234");
 	
-		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user2);
+		Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+		Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
+		Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, user2);
 		
 		userRepository.saveAll(Arrays.asList(user1, user2));
 		orderRepository.saveAll(Arrays.asList(order1, order2, order3));
